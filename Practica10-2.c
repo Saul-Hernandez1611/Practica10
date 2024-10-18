c8dace8796defbc36e1414f3484cc43765329001
Saul-Hernandez1611 <saul56delizbeth@gmail.com>
2024-10-18 14:17:12 -0600

    Practica10-2.c

---
+++ Practica10-2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <stdio.h>
+
+int main()
+{
+  int matriz[3][3]= {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}}; 
+  int i = 1, j = 1;
+
+  while(i <= 3)
+  {
+    while(j <= 3)
+    {
+        printf("%d ", matriz[i-1][j-1]);
+        j = j + 1;
+    }
+  printf("\n");
+  j = 0;
+  i = i + 1;
+  }
+return 0;
+}
